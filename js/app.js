c13ed613102a87162a922ae6a350bf465b0a9c49
mariokarajallo <mario_karajallo@hotmail.com>
2022-09-19 23:02:41 -0400

    definiendo constantes

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//! VARIABLES y SELECTORES
+const formulario = document.querySelector("#agregar-gasto");
+const gastoListado = document.querySelector("#gastos ul");
+
+//! EVENTOS
+
+//! CLASSES
+
+//! FUNCIONES
