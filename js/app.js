cba6016dbea9fbad35d3d655d0e1dc2e68921d4a
mariokarajallo <mario_karajallo@hotmail.com>
2022-09-19 23:13:48 -0400

    funcion preguntarPresupuesto

--- js/app.js
+++ js/app.js
@@ -3,7 +3,29 @@ const formulario = document.querySelector("#agregar-gasto");
 const gastoListado = document.querySelector("#gastos ul");
 
 //! EVENTOS
-
+eventListeners();
+function eventListeners() {
+  //al cargar todo el proyecto solicita el presupuesto
+  document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+}
 //! CLASSES
 
 //! FUNCIONES
+function preguntarPresupuesto() {
+  const presupuestoUsuario = prompt("Cual es tu presupuesto?");
+  console.log(presupuestoUsuario);
+
+  //validar prompt
+  if (
+    presupuestoUsuario === "" ||
+    presupuestoUsuario === null ||
+    isNaN(presupuestoUsuario) ||
+    presupuestoUsuario <= 0
+  ) {
+    window.location.reload();
+  }
+
+  // convierte cualquier numero que sea string a number
+
+  //recarga la pagina actual
+}
